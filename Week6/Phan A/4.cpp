da1bafa78b868997284d195eff7c94c3351e1da2
Ppt05 <160708311+Ppt05@users.noreply.github.com>
2024-05-29 14:05:32 +0700

    up

--- Week6/Phan A/4.cpp
+++ Week6/Phan A/4.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void testArray(int arr[]) {
+    cout << "Địa chỉ của mảng trong hàm testArray: " << arr << endl;
+}
+
+void testString(string str) {
+    cout << "Địa chỉ của string trong hàm testString: " << &str << endl;
+}
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    string str = "Hello";
+
+    testArray(arr);
+    testString(str);
     return 0;
 }
+
+
+    /*Nhận xét:
+    // - Mảng được truyền theo cơ chế pass-by-pointer. Khi một mảng được truyền vào một hàm, thực ra chỉ có con trỏ tới phần tử đầu tiên
+    của mảng được truyền, không phải là mảng thực sự. Do đó, mảng được truyền bằng cách tham chiếu, không phải tham trị.
+    // - std::string được truyền theo cơ chế pass-by-value. Mặc dù std::string trong C++ không phải là kiểu dữ liệu cơ bản nhưng được
+    thiết kế để hoạt động như một kiểu dữ liệu cơ bản, và mặc định được truyền bằng cách sao chép, nên thực sự được truyền theo cơ chế
+    pass-by-value
--- Week6/Phan A/5.cpp
+++ Week6/Phan A/5.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main() {
+    int a = 10;
+    int &b = a;
+
+
+    cout << "Giá trị của biến a: " << a << endl;
+    cout << "Giá trị của tham chiếu b " << b<< endl;
+
+
+    cout << "Địa chỉ của biến a: " << &a << endl;
+    cout << "Địa chỉ của tham chiếu b " << &b << endl;
+   return 0;
 }
+    /* Câu trả lời cho các câu hỏi:
+     - Biến tham chiếu và biến mà nó chiếu tới là cùng một biến trong bộ nhớ. Khi ta thay đổi giá trị của biến tham chiếu,
+    giá trị của biến gốc cũng thay đổi và ngược lại.
+     - Không thể khai báo một tham chiếu mà chưa chiếu nó tới một biến thường.
+     Một tham chiếu phải luôn được khởi tạo với việc chiếu tới một biến cụ thể ngay từ khi nó được khai báo.
+     - Có thể thay đổi đích của một tham chiếu sau khi nó đã được khởi tạo. Việc này sẽ làm thay đổi biến mà tham chiếu trước đó đang chiếu tới.*/
+
+
